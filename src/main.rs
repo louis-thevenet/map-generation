529ec1386c96f1f57ed41b51cba377ad26f0c755
Louis Thevenet <louis.tvnt@gmail.com>
2025-01-01 19:44:37 +0100

    feat: draw basic colors on elevation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use image::{ImageBuffer, Rgb};
 use rand::{seq::SliceRandom, thread_rng};
+use rayon::iter::ParallelIterator;
 struct Vector(f64, f64);
 impl Vector {
     fn dot_product(self, rhs: &Self) -> f64 {
@@ -22,42 +23,77 @@ fn fade(t: f64) -> f64 {
     (((6. * t - 15.) * t) + 10.) * t * t * t
 }
 
-#[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
+#[allow(
+    clippy::cast_sign_loss,
+    clippy::cast_possible_truncation,
+    clippy::cast_precision_loss,
+    clippy::similar_names
+)]
 fn perlin(permutations: &[usize], pos: (f64, f64)) -> f64 {
     let (x, y) = pos;
-    let (fx, fy) = (x.floor() as usize, y.floor() as usize);
+    let (nx, ny) = (pos.0 as usize, pos.1 as usize);
+    let (fx, fy) = (x - x.floor(), y - y.floor());
 
-    let TR = Vector((fx + 1) as f64, (fy + 1) as f64);
-    let TL = Vector(fx as f64, (fy + 1) as f64);
-    let BR = Vector((fx + 1) as f64, (fy) as f64);
-    let BL = Vector(fx as f64, (fy) as f64);
+    let tr = Vector(fx - 1.0, fy - 1.0);
+    let tl = Vector(fx, fy - 1.0);
+    let br = Vector(fx - 1.0, fy);
+    let bl = Vector(fx, fy);
 
-    let vTR = permutations[permutations[fx + 1] + fy + 1];
-    let vTL = permutations[permutations[fx] + fy + 1];
-    let vBR = permutations[permutations[fx + 1] + fy];
-    let vBL = permutations[permutations[fx] + fy];
+    let size = permutations.len();
+    let v_tr = permutations[(permutations[(nx + 1) % size] + (ny + 1) % size) % size];
+    let v_tl = permutations[(permutations[nx % size] + (ny + 1) % size) % size];
+    let v_br = permutations[(permutations[(nx + 1) % size] + ny % size) % size];
+    let v_bl = permutations[(permutations[nx % size] + ny % size) % size];
 
-    let dTR = TR.dot_product(&constant_vector(vTR));
-    let dTL = TL.dot_product(&constant_vector(vTL));
-    let dBR = BR.dot_product(&constant_vector(vBR));
-    let dBL = BL.dot_product(&constant_vector(vBL));
+    let d_tr = tr.dot_product(&constant_vector(v_tr));
+    let d_tl = tl.dot_product(&constant_vector(v_tl));
+    let d_br = br.dot_product(&constant_vector(v_br));
+    let d_bl = bl.dot_product(&constant_vector(v_bl));
 
-    let u = fade(fx as f64);
-    let v = fade(fy as f64);
-    lerp(u, lerp(v, dBL, dTL), lerp(v, dBR, dTR))
+    let u = fade(fx);
+    let v = fade(fy);
+
+    lerp(u, lerp(v, d_bl, d_tl), lerp(v, d_br, d_tr))
 }
+fn fractal_brownian_motion(permutations: &[usize], pos: (f64, f64), num_octaves: usize) -> f64 {
+    let mut result = 0.0;
+    let mut amplitude = 1.0;
+    let mut freq = 0.005;
+    for _ in 0..num_octaves {
+        let n = amplitude * perlin(permutations, (pos.0 * freq, pos.1 * freq));
+        result += n;
+        amplitude *= 0.5;
+        freq *= 2.0;
+    }
+    result
+}
+
 #[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
 fn main() {
-    let mut permutations: Vec<usize> = (0..=256).collect::<Vec<usize>>();
+    const GREEN: Rgb<u8> = Rgb([0, 255, 0]);
+    const BLUE: Rgb<u8> = Rgb([0, 0, 255]);
+    const WHITE: Rgb<u8> = Rgb([255, 255, 255]);
+    const YELLOW: Rgb<u8> = Rgb([255, 255, 0]);
+    let dimension = 1024 * 8;
+    let mut permutations: Vec<usize> = (0..=(dimension as usize)).collect::<Vec<usize>>();
     permutations.shuffle(&mut thread_rng());
-    let mut img = ImageBuffer::new(256, 256);
-    for y in 0..=255 {
-        for x in 0..=255 {
-            let pos = (f64::from(x) / 10., f64::from(y) / 10.);
-            let noise = (perlin(&permutations, pos) + 1.) / 2.;
-            let px = (noise * 255.) as u8;
-            img.put_pixel(x, y, Rgb([px, px, px]));
-        }
-    }
+    let mut img = ImageBuffer::new(dimension, dimension);
+    img.par_enumerate_pixels_mut().for_each(|(x, y, p)| {
+        let pos = (f64::from(x), f64::from(y));
+
+        let noise = (fractal_brownian_motion(&permutations, pos, 8) + 1.) / 2.;
+
+        let px = if noise > 0.85 {
+            WHITE
+        } else if noise > 0.5 {
+            GREEN
+        } else if noise > 0.48 {
+            YELLOW
+        } else {
+            BLUE
+        };
+
+        *p = px;
+    });
     let _ = img.save("output.png");
 }
