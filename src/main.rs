442cbe3a634bca2334fcf22636ae3ce55a4ba2e3
Louis Thevenet <louis.tvnt@gmail.com>
2025-01-02 09:30:11 +0100

    refactor: extract to lib

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,7 @@
 use image::{ImageBuffer, Rgb};
-use rand::{seq::SliceRandom, thread_rng};
+use perlin_gen::PerlinNoiseGenerator;
 use rayon::iter::ParallelIterator;
-struct Vector(f64, f64);
-impl Vector {
-    fn dot_product(self, rhs: &Self) -> f64 {
-        self.0 * rhs.0 + self.1 * rhs.1
-    }
-}
-fn constant_vector(h: usize) -> Vector {
-    match h % 4 {
-        0 => Vector(1., 1.),
-        1 => Vector(-1., 1.),
-        2 => Vector(-1., -1.),
-        _ => Vector(1., -1.),
-    }
-}
-fn lerp(t: f64, a1: f64, a2: f64) -> f64 {
-    a1 + t * (a2 - a1)
-}
-
-fn fade(t: f64) -> f64 {
-    (((6. * t - 15.) * t) + 10.) * t * t * t
-}
-
-#[allow(
-    clippy::cast_sign_loss,
-    clippy::cast_possible_truncation,
-    clippy::cast_precision_loss,
-    clippy::similar_names
-)]
-fn perlin(permutations: &[usize], pos: (f64, f64)) -> f64 {
-    let (x, y) = pos;
-    let (nx, ny) = (pos.0 as usize, pos.1 as usize);
-    let (fx, fy) = (x - x.floor(), y - y.floor());
-
-    let tr = Vector(fx - 1.0, fy - 1.0);
-    let tl = Vector(fx, fy - 1.0);
-    let br = Vector(fx - 1.0, fy);
-    let bl = Vector(fx, fy);
-
-    let size = permutations.len();
-    let v_tr = permutations[(permutations[(nx + 1) % size] + (ny + 1) % size) % size];
-    let v_tl = permutations[(permutations[nx % size] + (ny + 1) % size) % size];
-    let v_br = permutations[(permutations[(nx + 1) % size] + ny % size) % size];
-    let v_bl = permutations[(permutations[nx % size] + ny % size) % size];
-
-    let d_tr = tr.dot_product(&constant_vector(v_tr));
-    let d_tl = tl.dot_product(&constant_vector(v_tl));
-    let d_br = br.dot_product(&constant_vector(v_br));
-    let d_bl = bl.dot_product(&constant_vector(v_bl));
-
-    let u = fade(fx);
-    let v = fade(fy);
-
-    lerp(u, lerp(v, d_bl, d_tl), lerp(v, d_br, d_tr))
-}
-fn fractal_brownian_motion(permutations: &[usize], pos: (f64, f64), num_octaves: usize) -> f64 {
-    let mut result = 0.0;
-    let mut amplitude = 1.0;
-    let mut freq = 0.005;
-    for _ in 0..num_octaves {
-        let n = amplitude * perlin(permutations, (pos.0 * freq, pos.1 * freq));
-        result += n;
-        amplitude *= 0.5;
-        freq *= 2.0;
-    }
-    result
-}
+mod perlin_gen;
 
 #[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
 fn main() {
@@ -75,13 +10,12 @@ fn main() {
     const WHITE: Rgb<u8> = Rgb([255, 255, 255]);
     const YELLOW: Rgb<u8> = Rgb([255, 255, 0]);
     let dimension = 1024 * 8;
-    let mut permutations: Vec<usize> = (0..=(dimension as usize)).collect::<Vec<usize>>();
-    permutations.shuffle(&mut thread_rng());
+    let perlin_gen = PerlinNoiseGenerator::new(dimension as usize);
     let mut img = ImageBuffer::new(dimension, dimension);
     img.par_enumerate_pixels_mut().for_each(|(x, y, p)| {
         let pos = (f64::from(x), f64::from(y));
 
-        let noise = (fractal_brownian_motion(&permutations, pos, 8) + 1.) / 2.;
+        let noise = (perlin_gen.fractal_brownian_motion(pos, 8) + 1.) / 2.;
 
         let px = if noise > 0.85 {
             WHITE
---
+++ src/perlin_gen.rs
@@ -0,0 +1,82 @@
+use rand::{seq::SliceRandom, thread_rng};
+
+pub struct PerlinNoiseGenerator {
+    dimension: usize,
+    permutations: Vec<usize>,
+}
+impl PerlinNoiseGenerator {
+    pub fn new(dimension: usize) -> Self {
+        let mut permutations: Vec<usize> = (0..=(dimension)).collect::<Vec<usize>>();
+        permutations.shuffle(&mut thread_rng());
+        Self {
+            dimension,
+            permutations,
+        }
+    }
+
+    fn constant_vector(h: usize) -> Vector2 {
+        match h % 4 {
+            0 => Vector2(1., 1.),
+            1 => Vector2(-1., 1.),
+            2 => Vector2(-1., -1.),
+            _ => Vector2(1., -1.),
+        }
+    }
+    fn lerp(t: f64, a1: f64, a2: f64) -> f64 {
+        a1 + t * (a2 - a1)
+    }
+
+    fn fade(t: f64) -> f64 {
+        (((6. * t - 15.) * t) + 10.) * t * t * t
+    }
+    #[allow(
+        clippy::cast_sign_loss,
+        clippy::cast_possible_truncation,
+        clippy::cast_precision_loss,
+        clippy::similar_names
+    )]
+    fn perlin(&self, pos: (f64, f64)) -> f64 {
+        let (x, y) = pos;
+        let (nx, ny) = (pos.0 as usize, pos.1 as usize);
+        let (fx, fy) = (x - x.floor(), y - y.floor());
+
+        let tr = Vector2(fx - 1.0, fy - 1.0);
+        let tl = Vector2(fx, fy - 1.0);
+        let br = Vector2(fx - 1.0, fy);
+        let bl = Vector2(fx, fy);
+
+        let size = self.dimension;
+        let v_tr = self.permutations[(self.permutations[(nx + 1) % size] + (ny + 1) % size) % size];
+        let v_tl = self.permutations[(self.permutations[nx % size] + (ny + 1) % size) % size];
+        let v_br = self.permutations[(self.permutations[(nx + 1) % size] + ny % size) % size];
+        let v_bl = self.permutations[(self.permutations[nx % size] + ny % size) % size];
+
+        let d_tr = tr.dot_product(&Self::constant_vector(v_tr));
+        let d_tl = tl.dot_product(&Self::constant_vector(v_tl));
+        let d_br = br.dot_product(&Self::constant_vector(v_br));
+        let d_bl = bl.dot_product(&Self::constant_vector(v_bl));
+
+        let u = Self::fade(fx);
+        let v = Self::fade(fy);
+
+        Self::lerp(u, Self::lerp(v, d_bl, d_tl), Self::lerp(v, d_br, d_tr))
+    }
+    pub fn fractal_brownian_motion(&self, pos: (f64, f64), num_octaves: usize) -> f64 {
+        let mut result = 0.0;
+        let mut amplitude = 1.0;
+        let mut freq = 0.005;
+        for _ in 0..num_octaves {
+            let n = amplitude * self.perlin((pos.0 * freq, pos.1 * freq));
+            result += n;
+            amplitude *= 0.5;
+            freq *= 2.0;
+        }
+        result
+    }
+}
+struct Vector2(f64, f64);
+impl Vector2 {
+    fn dot_product(self, rhs: &Self) -> f64 {
+        self.0 * rhs.0 + self.1 * rhs.1
+    }
+}
