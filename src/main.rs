cdf705862ff6c10820d88d8d8e8a51fd1264612e
Louis Thevenet <louis.tvnt@gmail.com>
2025-01-04 19:22:40 +0100

    feat: draw chunk grid

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ fn main() {
     const BLUE: Rgb<u8> = Rgb([0, 0, 255]);
     const WHITE: Rgb<u8> = Rgb([255, 255, 255]);
     const YELLOW: Rgb<u8> = Rgb([255, 255, 0]);
-    let dimension = 1000;
-    let perlin_gen = PerlinNoiseGenerator::new(dimension as usize, Some(9_776_883_071_826_648_804))
+    const RED: Rgb<u8> = Rgb([255, 0, 0]);
+    let perlin_gen = PerlinNoiseGenerator::new(64, Some(4))
         .set_lacunarity(2.0)
         .set_persistance(0.5)
-        .set_octaves(8);
-    let image_gen = NoiseToImage::new(dimension)
+        .set_octaves(8)
+        .set_scale(40.0);
+    let image_gen = NoiseToImage::default()
         .add_layer(Layer {
             treshold: 0.85,
             color: WHITE,
@@ -33,8 +34,25 @@ fn main() {
             color: BLUE,
         });
 
-    let mut create_image = image_gen.create_image(40.0, &perlin_gen);
-    let px = create_image.get_pixel_mut(dimension / 2, dimension / 2);
-    *px = Rgb([255, 0, 0]);
-    let _ = create_image.save("output.png");
+    let chunk_to_draw = (2, 4);
+
+    // Draw chunks
+    let dimension = 8 * perlin_gen.chunk_size as u32;
+    let mut chunks_img = image_gen.create_image((dimension, dimension), &perlin_gen);
+    NoiseToImage::draw_grid(&mut chunks_img, perlin_gen.chunk_size as u32);
+    NoiseToImage::draw_rect(
+        &mut chunks_img,
+        (
+            (chunk_to_draw.0 * perlin_gen.chunk_size) as u32,
+            (chunk_to_draw.1 * perlin_gen.chunk_size) as u32,
+        ),
+        perlin_gen.chunk_size as u32,
+        perlin_gen.chunk_size as u32,
+        RED,
+    );
+    let _ = chunks_img.save("output.png");
+
+    // Draw one chunk
+    let mut single_chunk_img = image_gen.vec_to_image(&perlin_gen.generate_chunk(chunk_to_draw));
+    let _ = single_chunk_img.save("chunk_0_0.png");
 }
--- src/noise_to_img.rs
+++ src/noise_to_img.rs
@@ -7,34 +7,76 @@ pub struct Layer {
     pub treshold: f64,
     pub color: Rgb<u8>,
 }
+#[derive(Default)]
 pub struct NoiseToImage {
-    dimension: u32,
     layers: Vec<Layer>,
 }
 impl NoiseToImage {
-    pub fn new(dimension: u32) -> Self {
-        Self {
-            dimension,
-            layers: vec![],
+    pub fn draw_grid(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, cell_size: u32) {
+        const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
+        img.par_enumerate_pixels_mut().for_each(|(x, y, p)| {
+            if x % cell_size == 0 || y % cell_size == 0 {
+                *p = BLACK;
+            }
+        });
+    }
+    pub fn draw_rect(
+        img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
+        pos: (u32, u32),
+        width: u32,
+        height: u32,
+        color: Rgb<u8>,
+    ) {
+        const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
+        for x in 0..=width {
+            *img.get_pixel_mut(pos.0 + x, pos.1) = color;
+            *img.get_pixel_mut(pos.0 + x, pos.1 + height) = color;
+        }
+        for y in 0..=height {
+            *img.get_pixel_mut(pos.0, pos.1 + y) = color;
+            *img.get_pixel_mut(pos.0 + width, pos.1 + y) = color;
         }
     }
-
     pub fn add_layer(mut self, layer: Layer) -> Self {
         self.layers.push(layer);
+        self.layers
+            .sort_by(|l1, l2| l2.treshold.total_cmp(&l1.treshold));
         self
+    }
+    pub fn vec_to_image(&self, data: &[Vec<f64>]) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+        let mut img = ImageBuffer::new(data[0].len() as u32, data.len() as u32);
+        img.par_enumerate_pixels_mut().for_each(|(x, y, p)| {
+            let noise = (1.0 + data[y as usize][x as usize]) / 2.0;
+
+            // default pixel color is either lowest layer or noise map if layers is empty
+            let mut px = self
+                .layers
+                .last()
+                .unwrap_or(&Layer {
+                    treshold: 0.0,
+                    color: Rgb([(noise * 255.0) as u8; 3]),
+                })
+                .color;
+
+            for layer in &self.layers {
+                if noise >= layer.treshold {
+                    px = layer.color;
+                    break;
+                }
+            }
+            *p = px;
+        });
+        img
     }
     #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
     pub fn create_image(
-        mut self,
-        scale: f64,
+        &self,
+        size: (u32, u32),
         noise_gen: &PerlinNoiseGenerator,
     ) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-        self.layers
-            .sort_by(|l1, l2| l2.treshold.total_cmp(&l1.treshold));
-
-        let mut img = ImageBuffer::new(self.dimension, self.dimension);
+        let mut img = ImageBuffer::new(size.0, size.1);
         img.par_enumerate_pixels_mut().for_each(|(x, y, p)| {
-            let pos = (f64::from(x) / scale, f64::from(y) / scale);
+            let pos = (f64::from(x), f64::from(y));
 
             let noise = (noise_gen.noise(pos) + 1.) / 2.;
 
