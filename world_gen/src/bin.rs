5c2f9357641b2db39436826d4b3233f9634dbeae
Louis Thevenet <louis.tvnt@gmail.com>
2025-02-10 16:18:04 +0100

    feat(world_gen/bin): mark center of world

--- world_gen/src/bin.rs
+++ world_gen/src/bin.rs
@@ -4,7 +4,30 @@ use image::{ImageBuffer, Rgb};
 use progressing::{clamping, mapping, Baring};
 use world_gen::{chunk::Chunk, WorldGen};
 
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+// pub fn draw_grid(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, cell_size: u32) {
+//     const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
+//     img.par_enumerate_pixels_mut().for_each(|(x, y, p)| {
+//         if x % cell_size == 0 || y % cell_size == 0 {
+//             *p = BLACK;
+//         }
+//     });
+// }
+pub fn draw_rect(
+    img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
+    pos: (u32, u32),
+    width: u32,
+    height: u32,
+    color: Rgb<u8>,
+) {
+    for x in 0..=width {
+        *img.get_pixel_mut(pos.0 + x, pos.1) = color;
+        *img.get_pixel_mut(pos.0 + x, pos.1 + height) = color;
+    }
+    for y in 0..=height {
+        *img.get_pixel_mut(pos.0, pos.1 + y) = color;
+        *img.get_pixel_mut(pos.0 + width, pos.1 + y) = color;
+    }
+}
 fn save_maps(size: (u32, u32), chunks: &[Vec<Chunk>]) {
     let mut temp_img = ImageBuffer::new(size.0, size.1);
     let mut moisture_img = ImageBuffer::new(size.0, size.1);
@@ -47,22 +70,19 @@ fn save_maps(size: (u32, u32), chunks: &[Vec<Chunk>]) {
     erosion_img.save("erosion_map.png").unwrap();
 }
 
-fn save_biome_map(size: (u32, u32), chunks: &[Vec<Chunk>]) {
-    let mut biome_img = ImageBuffer::new(size.0, size.1);
-
+fn save_biome_map(biome_img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, chunks: &[Vec<Chunk>]) {
     for (x, chunk_row) in chunks.iter().enumerate() {
         for (y, chunk) in chunk_row.iter().rev().enumerate() {
             let color = Rgb(chunk.biome.color());
             biome_img.put_pixel(x as u32, y as u32, color);
         }
     }
-
-    biome_img.save("biome_map.png").unwrap();
 }
 
+#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let width = if args.len() > 1 {
+    let width: i64 = if args.len() > 1 {
         args[1].parse().unwrap()
     } else {
         1024
@@ -83,6 +103,14 @@ fn main() {
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
-    save_maps((width as u32, height as u32), &chunks);
-    save_biome_map((width as u32, height as u32), &chunks);
+    let mut biome_img = ImageBuffer::new(width as u32, height as u32);
+    save_biome_map(&mut biome_img, &chunks);
+    draw_rect(
+        &mut biome_img,
+        (width as u32 / 2 - 1, height as u32 / 2 - 1),
+        1,
+        1,
+        Rgb([255, 0, 0]),
+    );
+    biome_img.save("biome_map.png").unwrap();
 }
