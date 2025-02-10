a660355e131f8266c1b182dcda87a84b075a39cf
Louis Thevenet <louis.tvnt@gmail.com>
2025-02-10 15:41:55 +0100

    fix(world_gen/bin): reverse y axis of images

--- world_gen/src/bin.rs
+++ world_gen/src/bin.rs
@@ -1,33 +1,7 @@
 use std::env;
 
 use image::{ImageBuffer, Rgb};
-use rayon::iter::{ParallelBridge, ParallelIterator};
 use world_gen::{chunk::Chunk, WorldGen};
-
-// pub fn draw_grid(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, cell_size: u32) {
-//     const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
-//     img.par_enumerate_pixels_mut().for_each(|(x, y, p)| {
-//         if x % cell_size == 0 || y % cell_size == 0 {
-//             *p = BLACK;
-//         }
-//     });
-// }
-// pub fn draw_rect(
-//     img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
-//     pos: (u32, u32),
-//     width: u32,
-//     height: u32,
-//     color: Rgb<u8>,
-// ) {
-//     for x in 0..=width {
-//         *img.get_pixel_mut(pos.0 + x, pos.1) = color;
-//         *img.get_pixel_mut(pos.0 + x, pos.1 + height) = color;
-//     }
-//     for y in 0..=height {
-//         *img.get_pixel_mut(pos.0, pos.1 + y) = color;
-//         *img.get_pixel_mut(pos.0 + width, pos.1 + y) = color;
-//     }
-// }
 
 #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 fn save_maps(size: (u32, u32), chunks: &[Vec<Chunk>]) {
@@ -37,7 +11,7 @@ fn save_maps(size: (u32, u32), chunks: &[Vec<Chunk>]) {
     let mut erosion_img = ImageBuffer::new(size.0, size.1);
 
     for (x, chunk_row) in chunks.iter().enumerate() {
-        for (y, chunk) in chunk_row.iter().enumerate() {
+        for (y, chunk) in chunk_row.iter().rev().enumerate() {
             let temp_color = (255. * (chunk.temp + 1.) / 2.) as u8;
             // warm = red, cold = blue
             let color = Rgb([temp_color, 0, 255 - temp_color]);
@@ -76,9 +50,8 @@ fn save_biome_map(size: (u32, u32), chunks: &[Vec<Chunk>]) {
     let mut biome_img = ImageBuffer::new(size.0, size.1);
 
     for (x, chunk_row) in chunks.iter().enumerate() {
-        for (y, chunk) in chunk_row.iter().enumerate() {
+        for (y, chunk) in chunk_row.iter().rev().enumerate() {
             let color = Rgb(chunk.biome.color());
-
             biome_img.put_pixel(x as u32, y as u32, color);
         }
     }
@@ -97,13 +70,13 @@ fn main() {
     let height = width;
 
     let world_gen = WorldGen::default();
-    let chunks = (0..width)
+    let chunks = (-width / 2..width / 2)
         .map(|x| {
-            (0..height)
+            (-height / 2..height / 2)
                 .map(|y| world_gen.generate_chunk((x.try_into().unwrap(), y.try_into().unwrap())))
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
-    save_maps((width, height), &chunks);
-    save_biome_map((width, height), &chunks);
+    save_maps((width as u32, height as u32), &chunks);
+    save_biome_map((width as u32, height as u32), &chunks);
 }
